acffb9e5358d42bcc9465ff629bc7e9c3f948449
CNDOMAIN\abhishek.r <CNDOMAIN\abhishek.r@eb80e8db-569c-2843-a664-c13b63110e6a>
2008-12-11 09:08:24 +0000

    jira:HWE-2693- Reqster JsonCB Merge with AmsWebUi
    git-svn-id: http://projectserver/FrameWork/JsonClientBusiness/Trunk/JSONLayer/HiRePro.JSONServiceLayer/UserManagement@1494 eb80e8db-569c-2843-a664-c13b63110e6a

---
+++ DataContracts/JForgetPasswordResponse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Runtime.Serialization;
+using HiRePro.JSONServiceLayer.JSONInfrastructureServices.InfraDataContracts;
+
+namespace HiRePro.JSONServiceLayer.UserManagement
+{
+    [DataContract]
+    public class JForgetPasswordResponse : AjaxCommonCriteria
+    {
+        private string _hintQue;
+        private string _hintAns;
+        private int _userId;
+        private string _userName;
+
+        [DataMember]
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+        [DataMember]
+        public int UId
+        {
+            get { return _userId; }
+            set { _userId = value; }
+        }
+        [DataMember]
+        public string HintAns
+        {
+            get { return _hintAns; }
+            set { _hintAns = value; }
+        }
+        [DataMember]
+        public string HintQue
+        {
+            get { return _hintQue; }
+            set { _hintQue = value; }
+        }
+    }
+}
---
+++ DataContracts/JResetPasswordResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using HiRePro.JSONServiceLayer.JSONInfrastructureServices.InfraDataContracts;
+using System.Runtime.Serialization;
+
+namespace HiRePro.JSONServiceLayer.UserManagement
+{
+    [DataContract]
+    public class JResetPasswordResponse : AjaxCommonCriteria
+    {
+        private bool _isResetSuccessfully;
+
+        [DataMember]
+        public bool IsResetSuccessfully
+        {
+            get { return _isResetSuccessfully; }
+            set { _isResetSuccessfully = value; }
+        }
+    }
+}
