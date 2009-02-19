517dd9ce0ca8fbbb17e753813f6c1ea331399da8
cndomain\prateek.s <cndomain\prateek.s@eb80e8db-569c-2843-a664-c13b63110e6a>
2009-02-19 07:28:21 +0000

    jira:HWE-2921 Client Business update for Employee Referral in org
    git-svn-id: http://projectserver/FrameWork/JsonClientBusiness/Trunk/JSONLayer/HiRePro.JSONServiceLayer/UserManagement@1907 eb80e8db-569c-2843-a664-c13b63110e6a

---
+++ DataContracts/JsonChangePasswordRequest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using HiRePro.JSONServiceLayer.CommonManagement.DataContracts;
+
+namespace HiRePro.JSONServiceLayer.UserManagement.DataContracts
+{
+    [DataContract]
+    public class JsonChangePasswordRequest
+    {
+        #region Fields
+        private string _oldPassword;
+        private string _newPassword;
+        private JsonTenantAdamInfo tenantInfo;
+        #endregion
+
+        #region Properties
+
+
+        /// <summary>
+        /// Gets or sets the tenant info.
+        /// </summary>
+        /// <value>The tenant info.</value>
+        [DataMember]
+        public JsonTenantAdamInfo TenantInfo
+        {
+            get { return tenantInfo; }
+            set { tenantInfo = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the new password.
+        /// </summary>
+        /// <value>The new password.</value>
+        [DataMember]
+        public string NewPassword
+        {
+            get { return _newPassword; }
+            set { _newPassword = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the old password.
+        /// </summary>
+        /// <value>The old password.</value>
+        [DataMember]
+        public string OldPassword
+        {
+            get { return _oldPassword; }
+            set { _oldPassword = value; }
+        }
+
+        
+        #endregion
+
+    }
+}
---
+++ DataContracts/JsonChangePasswordResponse.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace HiRePro.JSONServiceLayer.UserManagement.DataContracts
+{
+    [DataContract]
+    public class JsonChangePasswordResponse
+    {
+        #region Fields
+        private bool _isPasswordChanged;
+        private string _errorMessage; 
+        #endregion
+
+        #region Properties
+
+
+        /// <summary>
+        /// Gets or sets the error message.
+        /// </summary>
+        /// <value>The error message.</value>
+        [DataMember]
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set { _errorMessage = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is password changed.
+        /// </summary>
+        /// <value>
+        /// 	<c>true</c> if this instance is password changed; otherwise, <c>false</c>.
+        /// </value>
+        [DataMember]
+        public bool IsPasswordChanged
+        {
+            get { return _isPasswordChanged; }
+            set { _isPasswordChanged = value; }
+        }
+
+
+        #endregion
+    }
+}
