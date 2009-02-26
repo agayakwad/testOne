9454faf01e740dfacecc3559b874b2ee6e576337
cndomain\palaksha.ks <cndomain\palaksha.ks@eb80e8db-569c-2843-a664-c13b63110e6a>
2009-02-26 10:43:21 +0000

    jira:HWE-3356 Salary field is exposed in JsonTestCandidate
    git-svn-id: http://projectserver/FrameWork/JsonClientBusiness/Trunk/JSONLayer/HiRePro.JSONServiceLayer/UserManagement@1962 eb80e8db-569c-2843-a664-c13b63110e6a

--- Translators/TranslateBetweenJsonActionModuleCollectionAndActionInfoType.cs
+++ Translators/TranslateBetweenJsonActionModuleCollectionAndActionInfoType.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using HiRePro.JSONServiceLayer.UserManagement.DataContracts;
 using Common.ServiceProxies.UserManagement;
+using Common.ServiceProxies.SystemService;
 
 namespace HiRePro.JSONServiceLayer.UserManagement.Translators
 {
     public static class TranslateBetweenJsonActionModuleCollectionAndActionInfoType
     {
-        public static List<JsonActionModule> ServiceToBusiness(ActionInfoType[] from)
+        public static List<JsonActionModule> ServiceToBusiness(Common.ServiceProxies.UserManagement.ActionType[] from)
         {
             List<JsonActionModule> to = new List<JsonActionModule>();
             List<int> moduleIds = new List<int>();
@@ -26,8 +27,8 @@ namespace HiRePro.JSONServiceLayer.UserManagement.Translators
                                 if (moduleIds.Contains(from[i].ModuleId))
                                 {
                                     JsonAction action = new JsonAction();
-                                    action.ActionName = from[i].ActionName;
-                                    action.ActionId = from[i].ActionId;
+                                    action.ActionName = from[i].Name;
+                                    action.ActionId = from[i].Id;
                                     to[j].ActionCollection.Add(action);
                                     break;
                                 }
@@ -37,8 +38,8 @@ namespace HiRePro.JSONServiceLayer.UserManagement.Translators
                                     actionModule.ModuleId = from[i].ModuleId;
                                     moduleIds.Add(from[i].ModuleId);
                                     JsonAction action = new JsonAction();
-                                    action.ActionName = from[i].ActionName;
-                                    action.ActionId = from[i].ActionId;
+                                    action.ActionName = from[i].Name;
+                                    action.ActionId = from[i].Id;
                                     actionModule.ActionCollection = new List<JsonAction>();
                                     actionModule.ActionCollection.Add(action);
                                     to.Add(actionModule);
@@ -53,8 +54,8 @@ namespace HiRePro.JSONServiceLayer.UserManagement.Translators
                             actionModule.ModuleId = from[i].ModuleId;
                             moduleIds.Add(from[i].ModuleId);
                             JsonAction action = new JsonAction();
-                            action.ActionName = from[i].ActionName;
-                            action.ActionId = from[i].ActionId;
+                            action.ActionName = from[i].Name;
+                            action.ActionId = from[i].Id;
                             actionModule.ActionCollection = new List<JsonAction>();
                             actionModule.ActionCollection.Add(action);
                             to.Add(actionModule);
