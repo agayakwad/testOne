2a10056a354c7c4f8de60756fdc3fcd347f0db66
CNDOMAIN\akash.v <CNDOMAIN\akash.v@eb80e8db-569c-2843-a664-c13b63110e6a>
2009-04-01 13:44:25 +0000

    jira:HWE-3619  Json Client Business For JsonRole (CreateRole)
    git-svn-id: http://projectserver/FrameWork/JsonClientBusiness/Trunk/JSONLayer/HiRePro.JSONServiceLayer/UserManagement@2185 eb80e8db-569c-2843-a664-c13b63110e6a

---
+++ DataContracts/JsonRole.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Runtime.Serialization;
+using HirePro.Framework.UI.Windows.Business;
+using HirePro.Framework.UI.Windows.Business.Base.Utility;
+using HiRePro.JSONServiceLayer.CommonManagement.DataContracts;
+using System.Collections.Generic;
+
+namespace HiRePro.JSONServiceLayer.UserManagement.DataContracts
+{
+    [DataContract]
+    public partial class JsonRole
+    {
+        #region Fields 
+        private string _name;
+        private string _description;
+      
+        #endregion
+
+        #region Properties 
+
+        [DataMember]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        [DataMember]
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        #endregion
+    }
+}
---
+++ Translators/TranslateBetweenJsonRoleAndRoleType.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using HiRePro.JSONServiceLayer.UserManagement.DataContracts;
+using Common.ServiceProxies.UserManagement;
+
+namespace HiRePro.JSONServiceLayer.UserManagement.Translators
+{
+    public static class TranslateBetweenJsonRoleAndRoleType
+    {
+        //public static CreateRoleRequest BusinessToService(JsonRole from)
+        //{
+        //    if (from != null)
+        //    {
+        //        CreateRoleRequest to = new CreateRoleRequest();
+        //        to.Role = new RoleType();
+        //        to.Role.Description = from.Description;
+        //        to.Role.Name = from.Name;
+                
+        //    }
+        //    return to;
+           
+        //}
+
+        public static JsonRole ServiceToBusiness(CreateRoleRequest from)
+        {
+            JsonRole to = new JsonRole();
+            if (from != null)
+            {
+                to.Description = from.Role.Description;
+                to.Name = from.Role.Name;
+               
+            }
+            return to;
+           
+        }
+
+    }
+}
