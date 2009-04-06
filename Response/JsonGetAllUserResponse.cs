f93d1d97457d6d5585164f62e7ec88eb5a06c052
cndomain\imran.khan <cndomain\imran.khan@eb80e8db-569c-2843-a664-c13b63110e6a>
2009-04-06 06:56:25 +0000

    Files Added for getallresponses   jira:HWE-3348
    git-svn-id: http://projectserver/FrameWork/JsonClientBusiness/Trunk/JSONLayer/HiRePro.JSONServiceLayer/UserManagement@2208 eb80e8db-569c-2843-a664-c13b63110e6a

---
+++ Response/JsonGetAllUserResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HiRePro.JSONServiceLayer.BaseResponse;
+using HiRePro.JSONServiceLayer.UserManagement.DataContracts;
+using System.Runtime.Serialization;
+
+namespace HiRePro.JSONServiceLayer.UserManagement.Response
+{  
+    [DataContract]
+    public class JsonGetAllUserResponse : ResponseBase
+    {
+        [DataMember]
+        public IList<JsonUser> JsonUserCollection
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public int RecordCount
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ Translators/TranslateBetweenJsonPagingCriteriaAndPagingCriteriaType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HiRePro.JSONServiceLayer.CommonManagement.DataContracts;
+using HirePro.Framework.Core.Utility.Enums;
+
+
+namespace HiRePro.JSONServiceLayer.UserManagement.Translators
+{
+    public static class TranslateBetweenJsonPagingCriteriaAndPagingCriteriaType
+    {
+
+        public static JsonPagingCriteria ServiceToBusiness(Common.ServiceProxies.UserManagement.PagingCriteriaType from)
+        {
+            JsonPagingCriteria to = new JsonPagingCriteria();
+            if (from != null)
+            {
+                to.MaxResults = from.MaxResults;
+                to.PageNo = from.PageNumber;
+                to.IsSpecificToUser = from.IsSpecificToUser;
+                to.ObjectState = EnumAdapter.GetEnumValue<ObjectState>(from.ObjectState);
+            }
+            return to;
+        }
+
+        public static Common.ServiceProxies.UserManagement.PagingCriteriaType BusinessToService(JsonPagingCriteria from)
+        {
+            Common.ServiceProxies.UserManagement.PagingCriteriaType to = new Common.ServiceProxies.UserManagement.PagingCriteriaType();
+            if (from != null)
+            {
+                to.MaxResults = from.MaxResults;
+                to.PageNumber = from.PageNo;
+                to.IsSpecificToUser = from.IsSpecificToUser;
+                to.ObjectState = EnumAdapter.GetEnumValue<Common.ServiceProxies.UserManagement.ObjectStateType>(from.ObjectState);
+            }
+            return to;
+        }
+
+    }
+}
