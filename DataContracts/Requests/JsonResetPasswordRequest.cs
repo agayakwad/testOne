f6a4b8c9ca8fe268c1334f3c54c4152c5cf52035
cndomain\himanshu.gupta <cndomain\himanshu.gupta@eb80e8db-569c-2843-a664-c13b63110e6a>
2009-09-09 10:13:01 +0000

    jira:HWE-5304
    token implementation
    
    git-svn-id: http://projectserver/FrameWork/JsonClientBusiness/Trunk/JSONLayer/HiRePro.JSONServiceLayer/UserManagement@3059 eb80e8db-569c-2843-a664-c13b63110e6a

--- DataContracts/Requests/JsonResetPasswordRequest.cs
+++ DataContracts/Requests/JsonResetPasswordRequest.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using HiRePro.JSONServiceLayer.CommonManagement.DataContracts;
 using System.Runtime.Serialization;
+using HiRePro.Utility.Cache;
+using HiRePro.JSONServiceLayer.Helper;
+using ClientBusiness.Services.Services;
 
 namespace HiRePro.JSONServiceLayer.UserManagement.DataContracts.Requests
 {
@@ -16,7 +19,9 @@ namespace HiRePro.JSONServiceLayer.UserManagement.DataContracts.Requests
         private int _userId;
         private string _newPassword;
         private int _tenantId;
+        private string _token;
         private bool _sendNotificationMail;
+        private static ICacheService Cache;
         #endregion
 
         #region Properties
@@ -46,11 +51,39 @@ namespace HiRePro.JSONServiceLayer.UserManagement.DataContracts.Requests
             get { return _tenantId; }
             set { _tenantId = value; }
         }
+
+        /// <summary>
+        /// Gets or sets the token.
+        /// </summary>
+        /// <value>The token.</value>
         [DataMember]
         public bool SendNotificationMail
         {
             get { return _sendNotificationMail; }
             set { _sendNotificationMail = value; }
+        }
+        [DataMember]
+        public string Token
+        {
+            get
+            {
+                if (TenantId != null && UserId != null)
+                {
+                    if (Cache == null)
+                    {
+                        Cache = new JsonEnterpriseLibraryCacheService();
+                    }
+                    int tenantID = int.Parse(HelperUtility.MD5Decrypt(TenantId.ToString()));
+                    int userID = int.Parse(HelperUtility.MD5Decrypt(UserId.ToString()));
+                    string tokenKey = "TokenKey" + tenantID + userID;
+                    if (Cache.GetData(tokenKey) != null)
+                    {
+                        _token = (string)Cache.GetData(tokenKey);
+                    }
+                }
+                return _token;
+            }
+            set { _token = value; }
         }
         #endregion
     }
