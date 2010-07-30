d4c71d1712d756387647f47b8616c28ac60d2139
cndomain\abhishek.s <cndomain\abhishek.s@eb80e8db-569c-2843-a664-c13b63110e6a>
2010-07-30 08:27:52 +0000

    jira:HWE-7647
    User Search implemented
    
    git-svn-id: http://projectserver/FrameWork/JsonClientBusiness/Trunk/JSONLayer/HiRePro.JSONServiceLayer/UserManagement@4626 eb80e8db-569c-2843-a664-c13b63110e6a

---
+++ DataContracts/Requests/JsonUserSearchRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using HiRePro.JSONServiceLayer.CommonManagement.DataContracts;
+
+namespace HiRePro.JSONServiceLayer.UserManagement.DataContracts.Requests
+{
+    [DataContract]
+    public class JsonUserSearchRequest : JsonTenantAdamInfo
+    {
+        [DataMember]
+        public JsonPagingCriteria PagingCriteria { get; set; }
+
+        [DataMember]
+        public TypeOfUser TypeOfUser { get; set; }
+
+        [DataMember]
+        public int UserNameId { get; set; }
+
+        [DataMember]
+        public string UserName { get; set; }
+
+        [DataMember]
+        public string Email { get; set; }
+
+
+
+    }
+}
---
+++ DataContracts/Response/JsonUserSearchResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HiRePro.JSONServiceLayer.BaseResponse;
+using System.Runtime.Serialization;
+
+namespace HiRePro.JSONServiceLayer.UserManagement.DataContracts.Response
+{
+    [DataContract]
+    public class JsonUserSearchResponse :ResponseBase
+    {
+
+        [DataMember]
+        public IList<JsonUser> JsonUserCollection
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public int RecordCount
+        {
+            get;
+            set;
+        }
+    }
+}
