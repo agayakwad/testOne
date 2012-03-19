74dca6809aaf72a82de315f4e40317cca491b0f8
CNDOMAIN\sourabh.kumar <CNDOMAIN\sourabh.kumar@eb80e8db-569c-2843-a664-c13b63110e6a>
2012-03-19 06:30:45 +0000

    jira:HWE-10367 Make Translator for GetAllUserName method.
    git-svn-id: http://projectserver/FrameWork/JsonClientBusiness/Trunk/JSONLayer/HiRePro.JSONServiceLayer/UserManagement@6576 eb80e8db-569c-2843-a664-c13b63110e6a

---
+++ Translators/TranslateBetweenJsonUsersNameRequestAndGetAllUserNamesRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HiRePro.JSONServiceLayer.UserManagement.DataContracts.Requests;
+using Common.ServiceProxies.UserManagement;
+using HirePro.Framework.Core.Utility.Enums;
+
+namespace HiRePro.JSONServiceLayer.UserManagement.Translators
+{
+   public static class TranslateBetweenJsonUsersNameRequestAndGetAllUserNamesRequest
+    {
+       public static GetAllUserNamesRequest BusinessToService(JsonUsersNameRequest from)
+       {
+           GetAllUserNamesRequest To = new GetAllUserNamesRequest();           
+           if (from != null)
+           {
+               To.IncludeDeleted = from.IsDeletedEntityBeIncluded;
+               To.IsUserRoleIsInterviewer = from.IsInterviewerOnly;
+               To.TypeOfUser = EnumAdapter.GetEnumValue<UserOfType>(from.TypeofUser);
+               if (from.IsDisabled.HasValue)
+               {
+                   To.IsDisabled = from.IsDisabled;
+               }
+               if (from.UsersExceptInTheseRolesIds != null && from.UsersExceptInTheseRolesIds.Length > 0)
+               {
+                   To.UsersExceptInTheseRolesIds = new int[from.UsersExceptInTheseRolesIds.Length];
+                   for (int i = 0; i < from.UsersExceptInTheseRolesIds.Length; i++)
+                   {
+                       To.UsersExceptInTheseRolesIds[i] = from.UsersExceptInTheseRolesIds[i];
+                   }
+               }
+               if (from.UsersInTheseRolesIds != null && from.UsersInTheseRolesIds.Length > 0)
+               {
+                   To.UsersInTheseRolesIds = new int[from.UsersInTheseRolesIds.Length];
+                   for (int i = 0; i < from.UsersInTheseRolesIds.Length; i++)
+                   {
+                       To.UsersInTheseRolesIds[i] = from.UsersInTheseRolesIds[i];
+                   }
+               }
+               
+           }
+           return To;
+       }
+
+    }
+}
