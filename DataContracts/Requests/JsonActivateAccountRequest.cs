67b92f01c9ee9f46351b3dac7e4953c5252275a5
CNDOMAIN\abhishek.s <CNDOMAIN\abhishek.s@eb80e8db-569c-2843-a664-c13b63110e6a>
2012-12-10 09:31:21 +0000

    jira:HWE-13726
    User Activation Process
    
    git-svn-id: http://projectserver/FrameWork/JsonClientBusiness/Trunk/JSONLayer/HiRePro.JSONServiceLayer/UserManagement@7379 eb80e8db-569c-2843-a664-c13b63110e6a

---
+++ DataContracts/Requests/JsonActivateAccountRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HiRePro.JSONServiceLayer.CommonManagement.DataContracts;
+using System.Runtime.Serialization;
+
+namespace HiRePro.JSONServiceLayer.UserManagement.DataContracts.Requests
+{
+    [DataContract]
+    public class JsonActivateAccountRequest : JsonTenantAdamInfo
+    {
+        [DataMember]
+        public string EmailId { get; set; }
+
+        [DataMember]
+        public bool SendActivationMail { get; set; } 
+    }
+}
---
+++ DataContracts/Response/JsonActivateAccountResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HiRePro.JSONServiceLayer.BaseResponse;
+using System.Runtime.Serialization;
+
+namespace HiRePro.JSONServiceLayer.UserManagement.DataContracts.Response
+{
+    [DataContract]
+    public class JsonActivateAccountResponse:ResponseBase 
+    {
+        [DataMember]
+        public string ActivationMessage {get;set;}
+       
+    }
+}
