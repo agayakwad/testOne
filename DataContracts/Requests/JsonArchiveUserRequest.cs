31e6b4b0937c137ff1e8b0e5ec841e47f56e8ea1
CNDOMAIN\abhishek.s <CNDOMAIN\abhishek.s@eb80e8db-569c-2843-a664-c13b63110e6a>
2012-12-11 05:35:45 +0000

    jira:HWE-13726
    Archive User work flow
    
    git-svn-id: http://projectserver/FrameWork/JsonClientBusiness/Trunk/JSONLayer/HiRePro.JSONServiceLayer/UserManagement@7382 eb80e8db-569c-2843-a664-c13b63110e6a

---
+++ DataContracts/Requests/JsonArchiveUserRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using HiRePro.JSONServiceLayer.CommonManagement.DataContracts;
+
+namespace HiRePro.JSONServiceLayer.UserManagement.DataContracts.Requests
+{
+    [DataContract]
+    public class JsonArchiveUserRequest : JsonTenantAdamInfo
+    {
+        [DataMember]
+        public bool IsArchived { get; set; }
+
+        [DataMember]
+        public int[] UserIds { get; set; } 
+
+    }
+}
