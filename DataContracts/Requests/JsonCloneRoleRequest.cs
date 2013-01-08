ed548089e78f8591e86a77249aa3098dd07489f9
CNDOMAIN\rajya.lakshmi <CNDOMAIN\rajya.lakshmi@eb80e8db-569c-2843-a664-c13b63110e6a>
2013-01-08 04:34:57 +0000

    jira:HWE-14034 Implement Clone Role in Web UI
    git-svn-id: http://projectserver/FrameWork/JsonClientBusiness/Trunk/JSONLayer/HiRePro.JSONServiceLayer/UserManagement@7448 eb80e8db-569c-2843-a664-c13b63110e6a

---
+++ DataContracts/Requests/JsonCloneRoleRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HiRePro.JSONServiceLayer.CommonManagement.DataContracts;
+using System.Runtime.Serialization;
+using HiRePro.JSONServiceLayer.CommonManagement.DataContracts.EventDataContracts;
+
+namespace HiRePro.JSONServiceLayer.UserManagement.DataContracts.Requests
+{
+    [DataContract]
+   public class JsonCloneRoleRequest:JsonTenantAdamInfo
+    {
+        [DataMember]
+        public int RoleId
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string RoleName
+        {
+        get;set;
+        }
+    }
+}
---
+++ DataContracts/Response/JsonCloneRoleResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using HiRePro.JSONServiceLayer.BaseResponse;
+
+namespace HiRePro.JSONServiceLayer.UserManagement.DataContracts.Response
+{
+    [DataContract]
+    public class JsonCloneRoleResponse:ResponseBase
+    {
+        [DataMember]
+        public string CloneRoleText
+        {
+            get;
+            set;
+        }
+    }
+}
